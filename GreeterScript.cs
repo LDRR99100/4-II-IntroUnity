efa6892042b18bd843b45d989e5316079155c1f3
alu0101129884 <56157468+LDRR99100@users.noreply.github.com>
2023-09-24 12:33:23 +0100

    Add files via upload

---
+++ GreeterScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreeterScript : MonoBehaviour
+{
+    private int frame = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("Hola! soy " + gameObject.name + "."); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Debug.Log(gameObject.name + " frame: " + frame);
+      frame++;
+    }
+}
